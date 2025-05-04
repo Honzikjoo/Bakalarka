1664b67b7efc61cf8122c40879670078c30de085
Honzikjoo <114592775+Honzikjoo@users.noreply.github.com>
2025-05-04 12:55:31 +0200

    Update test.py

--- bakalarka/test.py
+++ bakalarka/test.py
@@ -1,14 +1,10 @@
-
-import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 import pycomlink as pycml
 import xarray as xr
 import pandas as pd
 
 
-
-def load_data(data_path, start, end): #nacteni dat
+def load_data(data_path): #nacteni dat
     cmls = xr.open_dataset(data_path + '/example_cml_data.nc')
     return cmls
 
@@ -21,7 +17,7 @@ def load_radar(): #nacteni pomocnych dat z radaru
     radar_along_cml = xr.open_dataset(pycml.io.examples.get_example_data_path() + '/example_path_averaged_reference_data.nc')
     return radar_along_cml
 
-def preprocess_data(cmls, start, end): #zpracovani dat
+def preprocess_data(cmls): #zpracovani dat
 
     cml_list = [cmls.isel(cml_id=i) for i in range(len(cmls.cml_id))]
     cml = cml_list[0]
@@ -41,25 +37,25 @@ def preprocess_data(cmls, start, end): #zpracovani dat
     return cml #vraceni cml, snad staci takhle jako vraceni zpracovanych dat
 
 
-def waamethods(cml, i, start, end):#nastaveni jednotlivych metod, k cemu tu slouzi i ? mozna lepsi pouzit u cml = cml_list[i] ?
-    cml['waa_schleiss'] = pycml.processing.wet_antenna.waa_schleiss_2013(rsl=cml.trsl,baseline=cml.baseline,wet=cml.wet,waa_max=2.2,delta_t=1,tau=15,)
-    cml['waa_leijnse'] = pycml.processing.wet_antenna.waa_leijnse_2008_from_A_obs(A_obs=cml.A,f_Hz=cml.frequency,pol=cml.polarization,L_km=cml.length,)
-    cml['waa_pastorek'] = pycml.processing.wet_antenna.waa_pastorek_2021_from_A_obs(A_obs=cml.A,f_Hz=cml.frequency,pol=cml.polarization,L_km=cml.length,)
+def waamethod(cml, i, j):
+    if j=='schleiss':
+        cml['waa_schleiss'] = pycml.processing.wet_antenna.waa_schleiss_2013(rsl=cml.trsl,baseline=cml.baseline,wet=cml.wet,waa_max=2.2,delta_t=1,tau=15,)
+    if j=='leijnse':   
+        cml['waa_leijnse'] = pycml.processing.wet_antenna.waa_leijnse_2008_from_A_obs(A_obs=cml.A,f_Hz=cml.frequency,pol=cml.polarization,L_km=cml.length,)
+    if j=='pastorek':
+        cml['waa_pastorek'] = pycml.processing.wet_antenna.waa_pastorek_2021_from_A_obs(A_obs=cml.A,f_Hz=cml.frequency,pol=cml.polarization,L_km=cml.length,)
 
-    for waa_method in ['leijnse', 'pastorek', 'schleiss']:
-        cml[f'A_rain_{waa_method}'] = cml.trsl - cml.baseline - cml[f'waa_{waa_method}']
-        cml[f'A_rain_{waa_method}'] = cml[f'A_rain_{waa_method}'].where(cml[f'A_rain_{waa_method}'] >= 0, 0)
-        cml[f'R_{waa_method}'] = pycml.processing.k_R_relation.calc_R_from_A(
-        A=cml[f'A_rain_{waa_method}'], L_km=float(cml.length), f_GHz=cml.frequency/1e9, pol=cml.polarization
-        )
+    cml[f'A_rain_{j}'] = cml.trsl - cml.baseline - cml[f'waa_{j}']
+    cml[f'A_rain_{j}'] = cml[f'A_rain_{j}'].where(cml[f'A_rain_{j}'] >= 0, 0)
+    cml[f'R_{j}'] = pycml.processing.k_R_relation.calc_R_from_A(A=cml[f'A_rain_{j}'], L_km=float(cml.length), f_GHz=cml.frequency/1e9, pol=cml.polarization)
         
     cml['R'] = pycml.processing.k_R_relation.calc_R_from_A(A=cml.trsl - cml.baseline, L_km=float(cml.length), f_GHz=cml.frequency/1e9, pol=cml.polarization)
     return cml #opet vraceni cml, snad se ukladaji zmenena data takto
 
 
-def plot_waa_results(cml, radar_along_cml): #vykresleni grafu (nefunkcni)
-    fig, axs = plt.subplots(3, 1, figsize=(18, 10), sharex=True) #pise fig is not accessed
-    plt.sca(axs[0])
+def plot_waa_results(cml, radar_along_cml, i):
+    fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True) #pise fig is not accessed
+    plt.sca(axs)
     cml.trsl.isel(channel_id=0).plot.line(x='time', label='TRSL', color='k', zorder=10)
     cml.baseline.isel(channel_id=0).plot.line(x='time', label='baseline', color='C0')
     (cml.baseline + cml.waa_leijnse).isel(channel_id=0).plot.line(x='time', label='baseline + WAA_leijnse', color='C1')
@@ -68,8 +64,11 @@ def plot_waa_results(cml, radar_along_cml): #vykresleni grafu (nefunkcni)
     plt.ylabel('total path attenuation in dB')
     plt.title(f'cml_id = {cml.cml_id}   length = {cml.length:2.2f} km   frequency = {cml.frequency.isel(channel_id=0)/1e9} GHz')
     plt.legend()
+    axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
+    plt.show()
 
-    plt.sca(axs[1])
+    fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True)
+    plt.sca(axs)
     (radar_along_cml.sel(cml_id=cml.cml_id.values).rainfall_amount * 12).plot.line(color='k', linewidth=3.0, label='RADKLIM-YW', alpha=0.3)
     cml.R.isel(channel_id=0).plot.line(x='time', label='no WAA', color='C0')
     cml.R_leijnse.isel(channel_id=0).plot.line(x='time', label='with WAA_leijnse', color='C1')
@@ -78,8 +77,11 @@ def plot_waa_results(cml, radar_along_cml): #vykresleni grafu (nefunkcni)
     plt.ylabel('Rain-rate in mm/h')
     plt.title('')
     plt.legend()
+    axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
+    plt.show()
 
-    plt.sca(axs[2])
+    fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True)
+    plt.sca(axs)
     radar_along_cml.sel(cml_id=cml.cml_id.values).rainfall_amount.cumsum(dim='time').plot.line(color='k', linewidth=3.0, label='RADKLIM-YW', alpha=0.3)
     (cml.R.isel(channel_id=0)/60).cumsum(dim='time').plot.line(x='time', label='no WAA', color='C0')
     (cml.R_leijnse.isel(channel_id=0)/60).cumsum(dim='time').plot.line(x='time', label='with WAA_leijnse', color='C1')
@@ -88,32 +90,110 @@ def plot_waa_results(cml, radar_along_cml): #vykresleni grafu (nefunkcni)
     plt.ylabel('Rainfall sum in mm')
     plt.title('')
     plt.legend();
+    axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
     plt.show()
+
+
+def plot_waa_results_separate(cml, radar_along_cml, i, j):
+    if j == 'leijnse':
+        fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True) #pise fig is not accessed
+        plt.sca(axs)
+        cml.trsl.isel(channel_id=0).plot.line(x='time', label='TRSL', color='k', zorder=10)
+        cml.baseline.isel(channel_id=0).plot.line(x='time', label='baseline', color='C0')
+        (cml.baseline + cml.waa_leijnse).isel(channel_id=0).plot.line(x='time', label='baseline + WAA_leijnse', color='C1')
+        plt.ylabel('total path attenuation in dB')
+        plt.title(f'cml_id = {cml.cml_id}   length = {cml.length:2.2f} km   frequency = {cml.frequency.isel(channel_id=0)/1e9} GHz')
+        plt.legend()
+        axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
+        plt.show()
+
+        fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True)
+        plt.sca(axs)
+        (radar_along_cml.sel(cml_id=cml.cml_id.values).rainfall_amount * 12).plot.line(color='k', linewidth=3.0, label='RADKLIM-YW', alpha=0.3)
+        cml.R.isel(channel_id=0).plot.line(x='time', label='no WAA', color='C0')
+        cml.R_leijnse.isel(channel_id=0).plot.line(x='time', label='with WAA_leijnse', color='C1')
+        plt.ylabel('Rain-rate in mm/h')
+        plt.title('')
+        plt.legend()
+        axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
+        plt.show()
+
+        fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True)
+        plt.sca(axs)
+        radar_along_cml.sel(cml_id=cml.cml_id.values).rainfall_amount.cumsum(dim='time').plot.line(color='k', linewidth=3.0, label='RADKLIM-YW', alpha=0.3)
+        (cml.R.isel(channel_id=0)/60).cumsum(dim='time').plot.line(x='time', label='no WAA', color='C0')
+        (cml.R_leijnse.isel(channel_id=0)/60).cumsum(dim='time').plot.line(x='time', label='with WAA_leijnse', color='C1')
+        plt.ylabel('Rainfall sum in mm')
+        plt.title('')
+        plt.legend();
+        axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
+        plt.show()
+
+    if j == 'pastorek':
+        fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True) #pise fig is not accessed
+        plt.sca(axs)
+        cml.trsl.isel(channel_id=0).plot.line(x='time', label='TRSL', color='k', zorder=10)
+        cml.baseline.isel(channel_id=0).plot.line(x='time', label='baseline', color='C0')
+        (cml.baseline + cml.waa_leijnse).isel(channel_id=0).plot.line(x='time', label='baseline + WAA_leijnse', color='C1')
+        (cml.baseline + cml.waa_pastorek).isel(channel_id=0).plot.line(x='time', label='baseline + WAA_pastorek', color='C2')
+        plt.ylabel('total path attenuation in dB')
+        plt.title(f'cml_id = {cml.cml_id}   length = {cml.length:2.2f} km   frequency = {cml.frequency.isel(channel_id=0)/1e9} GHz')
+        plt.legend()
+        axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
+        plt.show()
+
+        fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True)
+        plt.sca(axs)
+        (radar_along_cml.sel(cml_id=cml.cml_id.values).rainfall_amount * 12).plot.line(color='k', linewidth=3.0, label='RADKLIM-YW', alpha=0.3)
+        cml.R.isel(channel_id=0).plot.line(x='time', label='no WAA', color='C0')
+        cml.R_leijnse.isel(channel_id=0).plot.line(x='time', label='with WAA_leijnse', color='C1')
+        cml.R_pastorek.isel(channel_id=0).plot.line(x='time', label='with WAA_pastorek', color='C2')
+        plt.ylabel('Rain-rate in mm/h')
+        plt.title('')
+        plt.legend()
+        axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
+        plt.show()
+
+        fig, axs = plt.subplots(1, figsize=(18, 10), sharex=True)
+        plt.sca(axs)
+        radar_along_cml.sel(cml_id=cml.cml_id.values).rainfall_amount.cumsum(dim='time').plot.line(color='k', linewidth=3.0, label='RADKLIM-YW', alpha=0.3)
+        (cml.R.isel(channel_id=0)/60).cumsum(dim='time').plot.line(x='time', label='no WAA', color='C0')
+        (cml.R_leijnse.isel(channel_id=0)/60).cumsum(dim='time').plot.line(x='time', label='with WAA_leijnse', color='C1')
+        (cml.R_pastorek.isel(channel_id=0)/60).cumsum(dim='time').plot.line(x='time', label='with WAA_pastorek', color='C2')
+        plt.ylabel('Rainfall sum in mm')
+        plt.title('')
+        plt.legend();
+        axs.set_xlim(pd.to_datetime('2018-05-'+str(i)+' 00:00:00'), pd.to_datetime('2018-05-'+str(i)+' 23:59:59'));
+        plt.show()
+        
+            
 
 
 
 def main():
-    waa_methods=['waa_schleiss','waa_leijnse','waa_pastorek'] #jen seznam metod ?
+    waa_methods=['waa_schleiss','waa_leijnse','waa_pastorek']
 
-    start = '2018-05-01'
-    end = '2018-05-30'
+    start = '2018-05-10'
+    end = '2018-05-20'
 
     data_path = pycml.io.examples.get_example_data_path()
 
-    cmls = load_data(data_path, start, end)
+    cmls = load_data(data_path)
 
     ref = load_ref(data_path)
 
     radar = load_radar()
 
-    cml = preprocess_data(cmls, start, end)
+    cml = preprocess_data(cmls)
 
     rains = {}
-    for i in waa_methods:
-        result_waa = waamethods(cml, i, start, end)
-        rains[i] = result_waa #k cemu je zde tato cast jen okopirovane z emailu - mozna nejaka mnozina kam se ukladaji vysledky?
-        plot_waa_results(result_waa, radar)
+    for i in range(14,15):
+        for j in ['leijnse', 'pastorek', 'schleiss']:
+            result_waa = waamethod(cml, i, j)
+            rains[j] = result_waa
+            plot_waa_results_separate(result_waa, radar, i, j)
+        plot_waa_results(result_waa, radar, i)
 
 
 
-main
+main()
